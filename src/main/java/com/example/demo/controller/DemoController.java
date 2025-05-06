ed5bf42e37d8664f235ade35581ce6258d551287
Shri <shrinarayantiwari990@gmail.com>
2025-05-06 10:04:02 +0530

    Day 3: Added UserController with @RequestBody, created User entity and service layer

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,49 +1,31 @@
 package com.example.demo.controller;
+import com.example.demo.entity.User;
+import com.example.demo.serviceImpl.UserServiceImpl;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class DemoController 
-{
 
-	@PostMapping("/userN")
+@RestController
+public class DemoController
+{
+	@Autowired
+	UserServiceImpl impl;
+	
+	@PostMapping("/userName")
 	public String getName()
 	{
-		return "Shri Narayan Tiwari" ;
+	return "Shri Narayan Tiwari";
 	}
 	
-	
-	@GetMapping("/userId")
-	public String getId(@RequestParam(required = false) String id)
+	@PostMapping("/create")
+	public String show(@RequestBody User user) 
 	{
-		List<String>  l1 = new ArrayList<>();
-		l1.add("1");
-		l1.add("2");
-		l1.add("3");
-		l1.add("4");
-		l1.add("5");
-		l1.add("6");
-		l1.add("7");
-		l1.add("8");
-		l1.add("9");
-		l1.add("10");
-		if(id != null)
-		{
-			for(String s: l1)
-			{
-				if(s.equals(id))
-					return s;
-			}
-			return "";
-		}
-		return l1.toString();
+	user = impl.createUser(user);
+	return user.toString();
 	}
 	
 }
+	
