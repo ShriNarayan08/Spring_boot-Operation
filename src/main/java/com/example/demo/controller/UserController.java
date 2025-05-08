57c19d38f2349d7f0a45717d9ed35aede8388fb6
Shri <shrinarayantiwari990@gmail.com>
2025-05-09 00:21:51 +0530

    Day 6:Diving Deeper into Spring Boot Internals

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,38 +5,31 @@ import com.example.demo.entity.User;
 import com.example.demo.exception.UserNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.serviceImpl.UserServiceImpl;
-
 @RestController
 @RequestMapping("/user")
 public class UserController 
 {
-
 	@Autowired
 	private UserServiceImpl userService;
-	
+
 	@PostMapping("/register")
-	public User createUser(@RequestBody User user){
-		
-		return userService.createUser(user);
+	public ResponseEntity<User> createUser(@RequestBody User user){
+
+		user= userService.createUser(user);
+		return new ResponseEntity<>(user , HttpStatus.ACCEPTED);
 	}
-	
+
 	@GetMapping("/all")
 	public List<User> getAllUsers(){
-		
+
 		return userService.getAllUsers();
 	}
-	
+
 	@PutMapping("/update/{id}")
 	public User updateUser(@PathVariable String id , @RequestBody User user){
 		
@@ -48,15 +41,10 @@ public class UserController
 		
 		return userService.deleteUser(id);
 	}
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
 	public String handleUserNotFoundException(UserNotFoundException ex) {
 		
 		return "Please Enter valid User Id";
-		
-		
 	}
-	
-	
-	
 }
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -4,15 +4,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.demo.utility.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(UserNotFoundException.class)
-	
 	public ResponseStatus handleUserNotFoundException(UserNotFoundException ex)
 	{
 		return new ResponseStatus(200,ex.getMessage());
+	}
+	@ExceptionHandler(NoResourceFoundException.class)
+	public String demo(NoResourceFoundException e)
+	{
+		return "Something went wrong";
 	}
 
 }
